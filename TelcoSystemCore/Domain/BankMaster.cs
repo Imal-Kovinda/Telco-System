dd61ea1afefda2734bc6708c14bf95266e5429bb
JackHallwalker <epcravisanka@gmail.com>
2022-12-05 15:25:12 +0530

    update

---
+++ TelcoSystemCore/Domain/BankMaster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelcoSystemCore.Common;
+
+namespace TelcoSystemCore.Domain
+{
+    [Serializable()]
+    public class BankMaster
+    {
+        [DBField("BANK_CODE")]
+        public String Bankode { get; set; }
+
+        [DBField("BANK_NAME")]
+        public String BankName { get; set; }
+
+        [DBField("INTRODUCED_BY")]
+        public String IntroducedBy { get; set; }
+
+        [DBField("INTRODUCED_ON")]
+        public String IntroducedOn { get; set; }
+
+        [DBField("CREDIT_CARD_ACCOUNT")]
+        public String CreditCardAccount { get; set; }
+
+        [DBField("MCASH_CODE")]
+        public String MCashCode { get; set; }
+
+
+    }
+}
